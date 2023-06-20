d866768567cbfeec06b8a06f0a0da113b90403d5
VelhoCat <diegopizarro2004@gmail.com>
2023-06-19 21:49:30 -0400

    Agrego reserva.py para comenzar la tarea

---
+++ Taller de Programacion/reserva.py
@@ -0,0 +1,6 @@
+""" Este programa utiliza diccionarios en vez de listas
+ya que es más flexible utilizar claves personalizadas.
+Podría por ejemplo utilizar prints para las llaves. """
+
+
+asientos = {}
