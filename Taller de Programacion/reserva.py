039fce9c883d753597f29203186825ff6522f672
VelhoCat <diegopizarro2004@gmail.com>
2023-06-19 21:58:39 -0400

    Arreglé el comentario que decía prints, por strings

--- Taller de Programacion/reserva.py
+++ Taller de Programacion/reserva.py
@@ -1,6 +1,6 @@
 """ Este programa utiliza diccionarios en vez de listas
 ya que es más flexible utilizar claves personalizadas.
-Podría por ejemplo utilizar prints para las llaves. """
+Podría por ejemplo utilizar strings para las llaves. """
 
 
 asientos = {}
