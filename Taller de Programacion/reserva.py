25874e769c982e77c8591ec1e534cf2fb5c5cd16
VelhoCat <diegopizarro2004@gmail.com>
2023-07-03 00:09:03 -0400

    Actualicé el programa sin la validación de rut

--- Taller de Programacion/reserva.py
+++ Taller de Programacion/reserva.py
@@ -3,17 +3,21 @@ ya que es más flexible utilizar claves personalizadas.
 Podría por ejemplo utilizar strings para las llaves. """
 
 
-asientos = {}
+from time import sleep
 
+asientos = {}
+CANT_ASIENTOS = 100
 def Datos_Asientos():
     asiento = {}
     EDAD_MIN = 4
     EDAD_MAX = 100
-    contador = 0
-    ASIENTO_NIÑO = 2000
-    ASIENTO_ADULTO = 3000
-    ASIENTO_ADULTO_MAYOR = 2500
-    datos = []
+    GALERIA_NINO = 2000
+    GALERIA_ADULTO = 3000
+    GALERIA_ADULTO_MAYOR = 2500
+    PALCO_NINO = 3000
+    PALCO_ADULTO = 5000
+    PALCO_ADULTO_MAYOR = 4000
+    precio = 0
     num_asiento = int(input("Elija su número de asiento: "))
     while num_asiento in asientos:
         print("Este asiento ya está ocupado!!")
@@ -23,16 +27,22 @@ def Datos_Asientos():
     edad = int(input("Ingrese su edad: "))
     while edad < EDAD_MIN or edad > EDAD_MAX:
         edad = int(input("Error, la edad debe ser mayor a {0} y menor a {1}. Ingrese su edad: ".format(EDAD_MIN,EDAD_MAX)))
-    if edad > EDAD_MIN and edad <= 18:
-        datos.append(ASIENTO_NIÑO)
-    if edad > 18 and edad <= 60:
-        datos.append(ASIENTO_ADULTO)
-    if edad > 60 and edad <= EDAD_MAX:
-        datos.append(ASIENTO_ADULTO_MAYOR)
+    if edad > EDAD_MIN and edad <= 18 and num_asiento > 20:
+        precio = GALERIA_NINO
+    elif edad > EDAD_MIN and edad <= 18 and num_asiento <= 20:
+        precio = PALCO_NINO
+    if edad > 18 and edad <= 60 and num_asiento > 20:
+        precio = GALERIA_ADULTO
+    elif edad > 18 and edad <= 60 and num_asiento <= 20:
+        precio = PALCO_ADULTO
+    if edad > 60 and edad <= EDAD_MAX and num_asiento > 20:
+        precio = GALERIA_ADULTO_MAYOR
+    elif edad > 60 and edad <= EDAD_MAX and num_asiento <= 20:
+        precio = PALCO_ADULTO_MAYOR
     asiento["Rut"] = rut
     asiento["Nombre"] = nombre
     asiento["Edad"] = edad
-    asiento["Precio"] = datos[contador]
+    asiento["Precio"] = precio
     asientos[num_asiento] = asiento
     print("Asiento reservado")
     print("Los datos del asiento comprado son:")
@@ -41,36 +51,23 @@ def Datos_Asientos():
     print("Nombre:", asiento["Nombre"])
     print("Edad:", asiento["Edad"])
     print("Monto a pagar:", asiento["Precio"])
-    contador += 1
+    
+def Asientos_Disponibles():
+    print("Asientos disponibles:")
+    for i in range(1,CANT_ASIENTOS + 1):
+        print(i)
+    sleep(2)
+    
+def Asientos_Ocupados():
+    print("Asientos ocupados:")
+    for i in asientos.keys():
+        print(i)
+    sleep(2)
 
-def Menu():
-    print("Bienvenido, para comprar un asiento, seleccione una de las siguientes opciones")
-    print("1: Comprar asiento")
-    print("2: Reembolsar asiento")
-    print("S: Salir")
-
-def Opciones(opcion):
-    FINALIZAR = "s"
-    FINAL = FINALIZAR.upper()
-    OP = opcion.upper()
-    if OP == FINAL:
-        print("Programa finalizado con éxito.")
-        exit()
-        
-    while opcion != FINALIZAR:
-        if opcion == "1":
-            Datos_Asientos()
-        elif opcion == "2":
-            Reembolsar_Asiento()
-        else:
-            print("Opción inválida. Por favor, seleccione una opción válida.")
-        Menu()
-        opcion = str(input("Opción a elegir: "))
-        Opciones(opcion)
-
-def Sumar_Precios():
+def Total_Recaudado():
     sumatoria = sum(asiento["Precio"] for asiento in asientos.values())
-    return sumatoria
+    print("El total recaudado es: " ,sumatoria)
+    sleep(2)
 
 def Reembolsar_Asiento():
     rut_reembolso = input("Ingrese el rut de la persona cuyo asiento desea reembolsar: ")
@@ -90,6 +87,40 @@ def Reembolsar_Asiento():
         print("Asiento reembolsado con éxito.")
     else:
         print("No se encontró ningún asiento asociado al rut ingresado.")
+    sleep(2)
+def Menu():
+    print("Bienvenido, para comprar un asiento, seleccione una de las siguientes opciones")
+    print("1: Comprar asiento")
+    print("2: Reembolsar asiento")
+    print("3: Asientos Disponibles")
+    print("4: Asientos Ocupados")
+    print("5: Total Recaudado")
+    print("S: Salir")
+
+def Opciones(opcion):
+    FINALIZAR = "s"
+    FINAL = FINALIZAR.upper()
+    OP = opcion.upper()
+    if OP == FINAL:
+        print("Programa finalizado con éxito.")
+        exit()
+        
+    while opcion != FINALIZAR:
+        if opcion == "1":
+            Datos_Asientos()
+        elif opcion == "2":
+            Reembolsar_Asiento()
+        elif opcion == "3":
+            Asientos_Disponibles()
+        elif opcion == "4":
+            Asientos_Ocupados()
+        elif opcion == "5":
+            Total_Recaudado()
+        else:
+            print("Opción inválida. Por favor, seleccione una opción válida.")
+        Menu()
+        opcion = str(input("Opción a elegir: "))
+        Opciones(opcion)
 
 Menu()
 opcion = str(input("Opción a elegir: "))
